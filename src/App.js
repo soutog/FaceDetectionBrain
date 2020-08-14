e1141c84dfd894ca491deb9659e917f97ed49b38
soutog <soutog4@gmail.com>
2020-08-14 19:46:11 -0300

    Front1

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number:{
+      value:100,
+      desinty:{
+        enable: true,
+        value_area:800
+      }
+    }
+  }
+}
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      input: ' ', 
+
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');  
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
